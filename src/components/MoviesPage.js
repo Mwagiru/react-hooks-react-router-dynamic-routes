701cfba47aa0f690b06d69c4368e317221a63bba
Mwagiru <joymwagiru@gmail.com>
2022-05-28 08:57:16 +0300

    Completed Assignment

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import MoviesList from "./MoviesList";
+import MoviesList from "./MoviesList.js";
 
 function MoviesPage({ movies }) {
   return (
